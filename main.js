9ab3246de31b9bf8230d4fd755a99993c15d9fe3
edwar-vhd <lyriard@gmail.com>
2022-10-17 21:05:15 -0500

    Cambios menú

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
 const nav_email = document.querySelector('.nav-email');
-const desktop_menu = document.querySelector('.desktop-menu ');
+const desktop_menu = document.querySelector('.desktop-menu');
+const menuHamb = document.querySelector('.menu');
+const mobile_menu = document.querySelector('.mobile-menu');
 
 nav_email.addEventListener('click', show_hide);
+menuHamb.addEventListener('click',toogleMenuHamb);
 
 function show_hide(){
     desktop_menu.classList.toggle('inactive');
+}
+
+function toogleMenuHamb(){
+    mobile_menu.classList.toggle('inactive');
+    mobile_menu.classList.toogle('mobile-menu');
 }
